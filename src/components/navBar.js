1a0bcf7c58e0ad5ae8e37dbb01f238bc8b9d306f
Alex Beloshevsky <alexbeloshevsky@gmail.com>
2018-10-02 12:23:01 +0300

    added the router component

---
+++ src/components/navBar.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class NavBar extends Component {
+
+  render() {
+    return (
+      <div id="navBar">
+      <Link to="/">Clients</Link>
+      <Link to="/actions">Actions</Link>
+      <Link to="/analytics">Analytics</Link>
+      </div> 
+    )
+  }
+
+}
+
+
+export default NavBar;
